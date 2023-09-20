a36c93f1612ce33c9ac972c1bc27a6e1dc082eb3
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-09-20 20:04:05 +0300

    Add project files.

---
+++ les_5/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les_5
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double balance)
+        {
+            if (balance < 0)
+            {
+                throw new ArgumentException("balance can't be negative");
+            }
+
+            this.balance = balance;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("deposit can't be negative");
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("withdraw can't be negative");
+            }
+
+            if (amount > balance)
+            {
+                throw new InsufficientFundsException("insufficient funds");
+            }
+
+            balance -= amount;
+        }
+
+        public void WithdrawMoney(double amount)
+        {
+            try
+            {
+                Withdraw(amount);
+                Console.WriteLine($"withdraw {amount}, balance: {GetBalance()}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ les_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace les_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BankAccount account = new BankAccount(1000);
+                Console.WriteLine($"Balance: {account.GetBalance()}");
+
+                account.WithdrawMoney(500);
+                account.WithdrawMoney(-500);
+                account.WithdrawMoney(2000);
+
+                account.Deposit(200);
+                Console.WriteLine($"Balance: {account.GetBalance()}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
